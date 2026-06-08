b4c4da7e98fb94773769f7db21dcf385ce389c93
Pranjal Pandey <ppranjal573@gmail.com>
2026-06-08 12:34:06 +0530

    Add mathematical functions and physics class implementations with encapsulation

---
+++ python_files/y26_m6_d07.py
@@ -0,0 +1,8 @@
+import mymath
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+obj = mymath.Mathematical_functions()
+print(obj.add(num1, num2))
+print(obj.multiply(num1, num2))
