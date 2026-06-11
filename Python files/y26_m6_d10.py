0bbbff09b5e9f0fb61d26800822baaf62991565a
Pranjal Pandey <ppranjal573@gmail.com>
2026-06-12 01:08:33 +0530

    fix: remove unused parameters from radius_c and area_m methods in Circle and Polygon classes

--- Python files/y26_m6_d10.py
+++ Python files/y26_m6_d10.py
@@ -9,7 +9,7 @@ class Circle:
         self.radius = radius
         self.circumference = circumference
 
-    def radius_c(self, marvel):
+    def radius_c(self):
       return self.radius*12 / self.circumference 
     
 
@@ -17,9 +17,9 @@ class Polygon:
     def __init__(self, radius, circumference):
         self.area = Circle(radius, circumference)
 
-    def area_m(self, h):
+    def area_m(self):
         return self.area.radius_c()
 
-pc = Polygon()
+pc = Polygon(radius=20, circumference=60)
 # pc.area(30)
 print(pc.area_m())
